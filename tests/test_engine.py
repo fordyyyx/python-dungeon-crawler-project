d2aeb02c36b40ef4143f2ffd0872cc41adf4a8fc
fordyyyx <127339634+fordyyyx@users.noreply.github.com>
2026-08-17 00:06:35 +0100

    fix spelling errors

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -5,40 +5,40 @@ from dungeon_crawler.engine import pick_up
 
 def test_pick_up_adds_item_to_inventory():
     room = Room("Armoury")
-    sword = Weapon("Bronze Xephis", "", damage=3)
+    sword = Weapon("Bronze Xiphos", "", damage=3)
     room.add_item(sword)
     player = Player(name="hero", hp=100)
 
-    pick_up(room, "bronze xephis", player)
+    pick_up(room, "bronze xiphos", player)
 
     assert sword in player.inventory.items
 
 def test_pick_up_removes_item_from_room():
     room = Room("Armoury")
-    sword = Weapon("Bronze Xephis", "", damage=3)
+    sword = Weapon("Bronze Xiphos", "", damage=3)
     room.add_item(sword)
     player = Player(name="hero", hp=100)
     assert sword in room.items
     
-    pick_up(room, "bronze xephis", player)
+    pick_up(room, "bronze xiphos", player)
     assert sword not in room.items
 
 def test_pick_up_returns_message_with_description(capsys):
     room = Room("Armoury")
-    sword = Weapon("Bronze Xephis", "", damage=3)
+    sword = Weapon("Bronze xiphos", "", damage=3)
     room.add_item(sword)
     player = Player(name="hero", hp=100)
 
-    print(pick_up(room, "bronze xephis", player))
+    print(pick_up(room, "bronze xiphos", player))
     captured = capsys.readouterr()
-    assert "You take the Bronze Xephis." in captured.out
+    assert "You take the Bronze xiphos." in captured.out
 
 
 def test_pick_up_returns_not_here_message_when_item_missing(capsys):
     room = Room("Armoury")
     player = Player(name="hero", hp=100)
 
-    print(pick_up(room, "bronze xephis", player))
+    print(pick_up(room, "bronze xiphos", player))
     captured = capsys.readouterr()
     assert "That's not here." in captured.out
 
